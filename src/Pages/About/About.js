56d573d4a849da7eae453a5a3678f895dd1d9340
sabiha-wasema <sabihawasemaa1244@gmail.com>
2022-05-07 20:49:02 +0600

    about added

---
+++ src/Pages/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageTitle from '../Shared/PageTitle/PageTitle';
+
+const About = () => {
+    return (
+        <div id="about">
+            <PageTitle title="About"></PageTitle>
+            <h1>This is about</h1>
+
+        </div>
+    );
+};
+
+export default About;
