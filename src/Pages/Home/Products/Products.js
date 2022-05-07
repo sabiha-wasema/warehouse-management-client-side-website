e2eadf193736a2833970784e81c8102604a1267d
sabiha-wasema <sabihawasemaa1244@gmail.com>
2022-05-07 20:54:19 +0600

    inventory section added

---
+++ src/Pages/Home/Products/Products.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import PageTitle from '../../Shared/PageTitle/PageTitle';
+import Product from '../Product/Product';
+import './Products.css';
+
+const Products = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, [])
+    return (
+        <div id="products" className='container mx-auto'>
+
+            <div className="row">
+                <PageTitle title="Products"></PageTitle>
+                <h1 className='text-dark text-center mt-5 mb-5'> Our Products</h1>
+                <div className="products-container">
+                    {
+                        products.map(product => <Product
+                            key={product.id}
+                            product={product}
+                        >
+                        </Product>)
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
